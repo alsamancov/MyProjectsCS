e488fdff5de1765101431824c8587a02d855c8fd
alsamancov <alsamancov@gmail.com>
2015-09-11 14:29:31 +0300

    third commit

--- Project1/Lib03/Lib03/Class1.cs
+++ Project1/Lib03/Lib03/Class1.cs
@@ -1,11 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Drawing;
+using System.Windows.Forms;
+using Autodesk.AutoCAD.Runtime;
+
+[assembly: CommandClass(typeof(Lib03.Wform))]
 
 namespace Lib03
 {
-    public class Class1
-    {
+    public class Wform : Form {
+
+        public Wform()
+        {
+            Text = "Modal dialog Book03";
+            Size = new Size(400, 200);
+            MaximumSize = new Size(600, 400);
+            BackColor = Color.BlanchedAlmond;
+        }
+
+        [CommandMethod("BOOK03")]
+        static public void book03() {
+            Application.Run(new Wform());
+        }
     }
 }
---
+++ Project1/Lib03/Lib04/Class1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.ApplicationServices;
+
+[assembly: CommandClass(typeof(Lib04.Book01))]
+[assembly: CommandClass(typeof(Lib04.Book02))]
+[assembly: CommandClass(typeof(Lib04.AddMyLayer))]
+[assembly: CommandClass(typeof(Lib04.IterateLayers))]
+
+namespace Lib04
+{
+    public class Book01
+    {
+        [CommandMethod("BOOK01")]
+        static public void book01() {
+            Transaction tr = HostApplicationServices.WorkingDatabase.TransactionManager.StartTransaction();
+            BlockTable bt = (BlockTable)tr.GetObject(HostApplicationServices.WorkingDatabase.BlockTableId, OpenMode.ForRead);
+            BlockTableRecord btr = (BlockTableRecord)tr.GetObject(bt[BlockTableRecord.ModelSpace], OpenMode.ForWrite);
+
+            Point3d[] pts = new Point3d[]{
+                new Point3d(15.0, 12.0, 0.0),
+                new Point3d(30.0, 44.0, 0.0),
+                new Point3d(60.0, 10.0, 0.0),
+                new Point3d(25.0, 30.0, 0.0)
+            };
+            Point3dCollection points = new Point3dCollection();
+            for (int i = 0; i < 4; i++) {
+                points.Add(pts[i]);
+            }
+            Spline spline = new Spline(points, 4, 0.0);
+
+            btr.AppendEntity(spline);
+            tr.AddNewlyCreatedDBObject(spline, true);
+
+            tr.Commit();
+            tr.Dispose();
+        }
+    }
+
+    public class Book02 {
+        [CommandMethod("BOOK02")]
+        static public void book02() {
+            Transaction tr = HostApplicationServices.WorkingDatabase.TransactionManager.StartTransaction();
+            BlockTable bt = (BlockTable)tr.GetObject(HostApplicationServices.WorkingDatabase.BlockTableId, OpenMode.ForRead);
+            BlockTableRecord btr = (BlockTableRecord)tr.GetObject(bt[BlockTableRecord.ModelSpace], OpenMode.ForWrite);
+
+            Point3d startPt = new Point3d(0, 0, 0);
+            Point3d endPt = new Point3d(10, 5, 0);
+
+            Line line = new Line(startPt, endPt);
+
+            btr.AppendEntity(line);
+            tr.AddNewlyCreatedDBObject(line, true);
+
+            tr.Commit();
+            tr.Dispose();
+        }
+    }
+
+    public class AddMyLayer {
+        [CommandMethod("AddMyLayer")]
+        static public void addMyLayer() {
+            Document acDoc = Application.DocumentManager.MdiActiveDocument;
+            Database acCurDb = acDoc.Database;
+
+            using (Transaction acTrans = acCurDb.TransactionManager.StartTransaction()) {
+                LayerTable acLyrTbl;
+                acLyrTbl = acTrans.GetObject(acCurDb.LayerTableId, OpenMode.ForRead) as LayerTable;
+
+                if (acLyrTbl.Has("MyLayer") != true) {
+                    acLyrTbl.UpgradeOpen();
+
+                    LayerTableRecord acLyrTblRec = new LayerTableRecord();
+                    acLyrTblRec.Name = "MyLayer";
+
+                    acLyrTbl.Add(acLyrTblRec);
+                    acTrans.AddNewlyCreatedDBObject(acLyrTblRec, true);
+
+                    acTrans.Commit();
+                }
+                acTrans.Dispose();
+            }
+        }
+    }
+
+    public class IterateLayers {
+        [CommandMethod("IterateLayers")]
+        public static void iterateLayers() {
+            Document acDoc = Application.DocumentManager.MdiActiveDocument;
+            Database acCurDb = acDoc.Database;
+
+            using (Transaction acTrans = acCurDb.TransactionManager.StartTransaction()) {
+                LayerTable acLyrTbl;
+                acLyrTbl = acTrans.GetObject(acCurDb.LayerTableId, OpenMode.ForRead) as LayerTable;
+
+                foreach (ObjectId acObjId in acLyrTbl) {
+                    LayerTableRecord acLyrTblRec;
+                    acLyrTblRec = acTrans.GetObject(acObjId, OpenMode.ForRead) as LayerTableRecord;
+
+                    acDoc.Editor.WriteMessage("\n" + acLyrTblRec.Name);
+                }
+                acTrans.Commit();
+                acTrans.Dispose();
+            }
+        }
+    }
+}
