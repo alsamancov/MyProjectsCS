edc8b59880e436a07e48c4a618ee989abcaf6fe8
alsamancov <alsamancov@gmail.com>
2015-09-11 15:59:41 +0300

    forth commit

---
+++ Project1/Lib03/Lib04/bin/Debug/Drawing1.dwl
@@ -0,0 +1,3 @@
+Алексей
+ALEKSEY_ 
+Friday, September 11, 2015  2:37:36 PM
---
+++ Project1/Lib03/Lib05/Class1.cs
@@ -0,0 +1,58 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Runtime;
+
+[assembly: CommandClass(typeof(Lib05.GetPointsFromUser))]
+
+namespace Lib05
+{
+    public class GetPointsFromUser
+    {
+        [CommandMethod("GetPointsFromUser")]
+        public static void getPointsFromUser(){
+            Document acDoc = Application.DocumentManager.MdiActiveDocument;
+            Database acCurDb = acDoc.Database;
+
+            PromptPointResult pPtRes;
+            PromptPointOptions pPtOpts = new PromptPointOptions("");
+
+            pPtOpts.Message = "\nEnter the start point of the line: ";
+            pPtRes = acDoc.Editor.GetPoint(pPtOpts);
+            Point3d ptStart = pPtRes.Value;
+
+            if(pPtRes.Status == PromptStatus.Cancel) return;
+
+            pPtOpts.Message = "\nEnter the end point of the line: ";
+            pPtOpts.UseBasePoint = true;
+            pPtOpts.BasePoint = ptStart;
+            pPtRes = acDoc.Editor.GetPoint(pPtOpts);
+            Point3d ptEnd = pPtRes.Value;
+
+            if(pPtRes.Status == PromptStatus.Cancel) return;
+
+            using (Transaction acTrans = acCurDb.TransactionManager.StartTransaction()){
+                BlockTable acBlkTbl;
+                BlockTableRecord acBlkTblRec;
+
+                acBlkTbl = acTrans.GetObject(acCurDb.BlockTableId, OpenMode.ForRead) as BlockTable;
+
+                acBlkTblRec = acTrans.GetObject(acBlkTbl[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
+
+                Line acLine = new Line(ptStart, ptEnd);
+                acLine.SetDatabaseDefaults();
+
+                acBlkTblRec.AppendEntity(acLine);
+                acTrans.AddNewlyCreatedDBObject(acLine, true);
+
+                acDoc.SendStringToExecute("._zoom _all ", true, false, false);
+
+                acTrans.Commit();
+
+
+
+            }
+        }
+    }
+}
